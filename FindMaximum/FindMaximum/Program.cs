e275e3326ae57445d3027aff2a29c3c87c39244d
nehak6296 <nehak6296@gmail.com>
2021-03-22 17:18:17 +0530

    [Neha]Add: Finding Maximum string number

--- FindMaximum/FindMaximum/Program.cs
+++ FindMaximum/FindMaximum/Program.cs
@@ -13,6 +13,9 @@ namespace FindMaximum
 
             double doubleOutput = MaximumNumberCheck.MaximumFloatNumber(111.44, 44.20, 33.65);
             Console.WriteLine(doubleOutput);
+
+            string stringOutput = MaximumNumberCheck.MaximumStringNumber("22","33","44");
+            Console.WriteLine(stringOutput);
         }
     }
 }
